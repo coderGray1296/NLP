08cae013e1fbd79250c2d18fdf5fa1efee696e83
coderGray <m13070193321@163.com>
2018-11-14 21:24:17 +0800

    summit

--- cnn-text-classification/CnnTextClassification/text_cnn.py
+++ cnn-text-classification/CnnTextClassification/text_cnn.py
@@ -18,7 +18,7 @@ class TextCNN(object):
         with tf.device('/cpu:0'), tf.name_scope("embedding"):
             self.W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),name="W")
             self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
-            #[batch_size, height, weight, input_channels] 其中batch_size为None
+            #[batch_size, height=sequence_size, weight=embedding_size, input_channels] 其中batch_size为None
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
         #creating convolution + maxpool for ever layer
