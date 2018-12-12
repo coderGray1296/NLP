d08a7fd2b460f557e37aff4aae325cde5436fc23
coderGray <m13070193321@163.com>
2018-12-12 17:42:33 +0800

    update

--- bi_lstm_crf/bi-lstm-crf/data_helper.py
+++ bi_lstm_crf/bi-lstm-crf/data_helper.py
@@ -86,7 +86,7 @@ def read_dictionary(vocab_path):
     """
         :param vocab_path:
         :return:
-        """
+    """
     vocab_path = os.path.join(vocab_path)
     with open(vocab_path, 'rb') as fr:
         word2id = pickle.load(fr)
@@ -99,6 +99,7 @@ def random_embedding(vocab, embedding_dim):
         :param embedding_dim:
         :return:
     """
+    #random parameters in gaosifenbu
     embedding_mat = np.random.uniform(-0.25, 0.25, (len(vocab), embedding_dim))
     embedding_mat = np.float32(embedding_mat)
     return embedding_mat
@@ -110,3 +111,36 @@ def pad_sequences(sequences, pad_mark=0):
         :return:
     """
     max_len = max(map(lambda x : len(x), sequences))
+    seq_list, seq_len_list = [], []
+    for seq in sequences:
+        seq = list(seq)
+        seq_ = seq[:max_len] + [pad_mark] * max(max_len - len(seq), 0)
+        seq_list.append(seq_)
+        seq_len_list.append(min(len(seq), max_len))
+    return seq_list, seq_len_list
+
+def batch_yield(data, batch_size, vocab, tag2label, shuffle=False):
+    """
+        :param data:
+        :param batch_size:
+        :param vocab:
+        :param tag2label:
+        :param shuffle:
+        :return:
+    """
+    if shuffle:
+        random.shuffle(data)
+    seqs, labels = [], []
+    for (sent_, tag_) in data:
+        sent_ = sentence2id(sent_, vocab)
+        label_ = [tag2label[tag] for tag in tag_]
+
+        if len(seqs) == batch_size:
+            yield seqs, labels
+            seqs, labels = [], []
+
+        seqs.append(sent_)
+        labels.append(label_)
+    if len(seqs) != 0:
+        yield seqs, labels
+
