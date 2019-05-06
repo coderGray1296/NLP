56f58c4912c56e60136b75dc900f80b563f48c9c
coderGray <m13070193321@163.com>
2019-05-06 20:38:06 +0800

    update

--- Target_Sentiment_Analysis/TSA/test.py
+++ Target_Sentiment_Analysis/TSA/test.py
@@ -1,2 +1,4 @@
-import nltk
-nltk.download('averaged_perceptron_tagger')
+import tensorflow as tf
+t1 = [[[1, 2], [2, 3], [5, 6]], [[4, 4], [5, 3], [7, 8]]]
+result = tf.concat(t1, axis=1)
+print(result.shape)
